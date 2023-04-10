e4f39c1c7e00088f486d1aa61eaa534f05f22410
Keerty Verma <keertyverma1303@gmail.com>
2023-04-10 16:44:45 +0530

    Add Reusable and Dynamic Table component

---
+++ src/components/Table.js
@@ -0,0 +1,29 @@
+export default function Table({ config, data, keyFn }) {
+  const renderedHeader = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td key={column.label} className="p-3">
+          {column.render(rowData)}
+        </td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeader}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
+}
---
+++ src/pages/TablePage.js
@@ -0,0 +1,31 @@
+import Table from "../components/Table";
+
+export default function TablePage() {
+  const config = [
+    {
+      label: "Fruits",
+      render: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+    },
+  ];
+
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
+  return <Table config={config} data={data} keyFn={keyFn} />;
+}
