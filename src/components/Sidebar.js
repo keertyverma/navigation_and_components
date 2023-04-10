96567def5b1c4eb1886707faeecae7e25fd2c6ec
Keerty Verma <keertyverma1303@gmail.com>
2023-04-10 15:47:45 +0530

    Add Modal component using Portal

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,7 @@ export default function Sidebar() {
     { label: "Dropdown", path: "/" },
     { label: "Accordion", path: "/accordion" },
     { label: "Button", path: "/button" },
+    { label: "Modal", path: "/modal" },
   ];
 
   const renderedLinks = links.map((link) => {
---
+++ src/pages/ModalPage.js
@@ -0,0 +1,36 @@
+import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
+
+export default function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is some important agreement!</p>
+    </Modal>
+  );
+  return (
+    <div>
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && modal}
+    </div>
+  );
+}
